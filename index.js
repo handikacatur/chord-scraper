2d63b4454651f9731bad7a938a22bf009f52ea9a
handikacatur <handika@rubyh.co>
2024-05-15 15:11:49 +0700

    add config example

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import yamlConfig from 'node-yaml-config';
 import startScrap from './scraper/index.js';
 import { Sequelize } from 'sequelize';
 import initModels from './model/init-models.js';
+import axios from 'axios';
+import chord from './model/chord.js';
 
 function getConfig() {
   return yamlConfig.load('./.config.yaml')
@@ -34,4 +36,33 @@ const sequelize = new Sequelize(`postgres://${DBUsername}:${DBPassword}@${DBHost
 
 initModels(sequelize)
 
-startScrap(config);
+const {artistName, songChords} = await startScrap(config)
+
+const client = axios.create({
+  baseURL: config['chordhub-api']['base-url'],
+  timeout: 30 * 1000,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+})
+
+
+async function uploadChord(artistName, songChords, client) {
+  const artist = await client.post('/artist', {
+    name: artistName
+  }).then(response => response.data)
+
+  songChords.forEach(async chord => {
+    await client.post('/chord', {
+      title: chord.title,
+      slug: chord.slug,
+      chord: chord.chord,
+      artist_id: artist.artist.id
+    })
+  });
+
+  console.log("\nChord succesfully uploaded!");
+}
+
+uploadChord(artistName, songChords, client)
